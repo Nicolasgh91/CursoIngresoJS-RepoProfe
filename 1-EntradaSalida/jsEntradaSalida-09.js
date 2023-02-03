f613ec66c05b61ad30208dbff7cbf1dd89fcdad9
Nicolasgh91 <gulley.erase0h@icloud.com>
2023-02-03 15:15:25 -0300

    Subo ejercicio E/S 9 bis

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,7 +6,7 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 
-function mostrarAumento()
+//function mostrarAumento()
 {
 	let sueldo;
 	let aumento;
@@ -17,5 +17,22 @@ function mostrarAumento()
 	nuevoSueldo = sueldo + aumento
  	
 	document.getElementById("txtIdResultado").value = nuevoSueldo
-	
+}
+
+/* 9 BIS : Debemos lograr tomar el importe por ID ,transformarlo a entero
+(parseInt), y tambien pedirle al usuario mediante una entrada de datos
+cual es el aumento a realizar, y luego mostrar el importe con el aumento
+ en el TextBox "RESULTADO". */
+function mostrarAumento()
+{
+	let importe;
+	let aumento;
+	let resultado;
+
+	importe = parseFloat(document.getElementById("txtIdSueldo").value);
+	aumento = parseFloat(prompt("Indique por favor el aumento de sueldo que desea obtener: "));
+
+	resultado = (importe / 100 * aumento) + importe;
+
+	document.getElementById("txtIdResultado").value = resultado;
 }
