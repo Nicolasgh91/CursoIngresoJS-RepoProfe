2a704038fe8dd74affb954c14ee3e8aa604279cf
Nicolasgh91 <gulley.erase0h@icloud.com>
2023-02-06 14:18:45 -0300

    Subo TP'S 2,3 y 4

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,44 +1,54 @@
-/*2.	Para el departamento de Construcción:
+/*
+	Alumno: Nicolás Gabriel Hruszczak
+	
+    Consigna:
+2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar si se ingresara el largo
     y el ancho de un terreno rectangular y se debe alambrar con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular
      y se debe alambrar con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal,
      debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
-*/
 
-let largoTerreno;
-let anchoTerreno;
-let radioTerreno;
-let resultado;
+     // Utilizar variables locales
+     */
+
+
 
 function Rectangulo () 
 {
+    let largoTerreno;
+    let anchoTerreno;
+    let resultado;
+
     largoTerreno = parseFloat(document.getElementById("txtIdLargo").value);
     anchoTerreno = parseFloat(document.getElementById("txtIdAncho").value);
-    radioTerreno = parseFloat(document.getElementById("txtIdRadio").value);
+    
     resultado = (largoTerreno * 2 + anchoTerreno * 2) * 3;
     alert(resultado.toFixed(2));
 }
 function Circulo () 
 {
-	largoTerreno = parseFloat(document.getElementById("txtIdLargo").value);
-    anchoTerreno = parseFloat(document.getElementById("txtIdAncho").value);
+    let radioTerreno;
+    let resultado;
+
     radioTerreno = parseFloat(document.getElementById("txtIdRadio").value);
     resultado = 2 * radioTerreno * Math.PI * 3;
 
-    alert(resultado.toFixed(2));
+    alert(resultado.toFixed(2)); 
 
 }
 function Materiales () 
 {
+    let largoTerreno;
+    let anchoTerreno;
+    let area;
+
 	largoTerreno = parseFloat(document.getElementById("txtIdLargo").value);
     anchoTerreno = parseFloat(document.getElementById("txtIdAncho").value);
-    radioTerreno = parseFloat(document.getElementById("txtIdRadio").value);
-    
-    let area = largoTerreno * anchoTerreno;
+    area = largoTerreno * anchoTerreno;
     const CEMENTO_X_METRO = area * 2; 
     const CAL_X_METRO = area * 3;
 
-    alert(`Para los ${area}m2 se van a necesitar ${CEMENTO_X_METRO} bolsas de cemento y ${CAL_X_METRO} bolsas de cal`)
+    alert(`Para los ${area} m2 se van a necesitar ${CEMENTO_X_METRO} bolsas de cemento y ${CAL_X_METRO} bolsas de cal`)
 }
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,79 +1,96 @@
-/*4.	Para el departamento de iluminación:
-Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40% y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
-     se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%,
-     si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos 
-    brutos en informar del impuesto con el siguiente mensaje:
-    ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+/*4.
+	Alumno: Nicolás Gabriel Hruszczak
+	
+    Consigna:
+    Para el departamento de iluminación:
+    Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+    
+    A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+    
+    B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40% y si es de otra marca el descuento es del 30%.
+   
+    C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
+        se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+    
+    D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%,
+        si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+    
+    E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos 
+        brutos en informar del impuesto con el siguiente mensaje:
+        ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
 {
- 	
+    let precioBaselamparas = 35;
     let marcaLamparas = document.getElementById("Marca").value;
     let cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
-    //let precioConDescuento = parseFloat(document.getElementById("txtIdprecioDescuento").value);
-    let precioBaselamparas = 35;
     let precioFinal = precioBaselamparas * cantidadLamparas;
     let descuento;
     let ingresosBrutos = precioFinal / 100 * 10;
     
     switch (cantidadLamparas) {
-        
-        case (cantidadLamparas >= 6 && marcaLamparas === "ArgentinaLuz"):
-            descuento = precioFinal / 100 * 50;
-            precioFinal -= descuento
-            parseFloat(document.getElementById("txtIdprecioDescuento").value) += precioFinal;
+        case 5:
+                if (marcaLamparas === "ArgentinaLuz") {
+                    descuento = precioFinal / 100 * 40;
+                    precioFinal -= descuento;
+                    alert(precioFinal);
+                } else {
+                    descuento = precioFinal / 100 * 30;
+                    precioFinal -= descuento;
+                    if(precioFinal > 120){
+                        precioFinal += ingresosBrutos;
+                        alert(`Usted pagó ${precioFinal} final, siendo ${ingresosBrutos} el importe de IIBB`);
+                    } else{
+                        alert(precioFinal);    
+                    }
+                }
             break;
-        case cantidadLamparas === 5 && marcaLamparas === "ArgentinaLuz":
-            descuento = precioFinal / 100 * 40;
-            precioFinal -= descuento;
-            alert(precioFinal)
+        case 4:
+            if (marcaLamparas === "ArgentinaLuz" || marcaLamparas === "FelipeLamparas") {
+                descuento = precioFinal / 100 * 25;
+                precioFinal -= descuento;
+                alert(precioFinal);
+            } else {
+                descuento = precioFinal / 100 * 20;
+                precioFinal -= descuento;
+                alert(precioFinal);
+            }
             break;
-        case cantidadLamparas === 5 && marcaLamparas != "ArgentinaLuz":
-            descuento = precioFinal / 100 * 30;
-            precioFinal -= descuento;
-            alert(precioFinal)
+        case 3:
+            if (marcaLamparas === "ArgentinaLuz") {
+                descuento = precioFinal / 100 * 15;
+                precioFinal -= descuento;
+                alert(precioFinal);
+            } else if (marcaLamparas === "FelipeLamparas"){
+                descuento = precioFinal / 100 * 10;
+                precioFinal -= descuento;
+                alert(precioFinal);
+            } else {
+                descuento = precioFinal / 100 * 5;
+                precioFinal -= descuento;
+                alert(precioFinal);    
+            }
             break;
-        case cantidadLamparas === 4 && marcaLamparas === "ArgentinaLuz" || marcaLamparas === "FelipeLamparas":
-            descuento = precioFinal / 100 * 25;
-            precioFinal -= descuento;
-            alert(precioFinal)
+        case 2:
+                alert(precioFinal);    
             break;
-        case cantidadLamparas === 4 && marcaLamparas != "ArgentinaLuz" || marcaLamparas != "FelipeLamparas":
-            descuento = precioFinal / 100 * 20;
-            precioFinal -= descuento;
-            alert(precioFinal)
+        case 1:
+                alert(precioFinal);    
             break;
-        case cantidadLamparas === 3 && marcaLamparas === "ArgentinaLuz":
-            descuento = precioFinal / 100 * 15;
-            precioFinal -= descuento;
-            alert(precioFinal)
-            break;
-        case cantidadLamparas === 3 && marcaLamparas === "FelipeLamparas":
-            descuento = precioFinal / 100 * 10;
-            precioFinal -= descuento;
-            alert(precioFinal)
-            break;
-        case cantidadLamparas === 3 && marcaLamparas != "ArgentinaLuz" || marcaLamparas != "FelipeLamparas": 
-            descuento = precioFinal / 100 * 5;
-            precioFinal -= descuento;
+        default:
+            if (cantidadLamparas >= 6) {
+                descuento = precioFinal / 100 * 50;
+                precioFinal -= descuento;
+                if(precioFinal > 120){
+                    precioFinal += ingresosBrutos;
+                    alert(`Usted pagó ${precioFinal} final, siendo ${ingresosBrutos} el importe de IIBB`);
+                } else{
+                    alert(precioFinal);    
+                }
+            } else {
+                alert("Compre al menos una lamparita, dale");
+            }
             break;
         }
-
-        /*
-            if(precioFinal > 120){
-            precioFinal += ingresosBrutos;
-            alert(`Usted pagó ${precioFinal} final, siendo ${ingresosBrutos} el importe de IIBB`);
-            }
-        */
-
-/*    E.	Si el importe final con descuento suma más de $120 se debe sumar un 10% de ingresos 
-    brutos e informar del impuesto con el siguiente mensaje:
-    ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-*/
 }
